88f28287591de931abfa194b96858d8c19a8abfb
anexr <anexr@HP>
2024-12-12 11:49:15 +0530

    interface

--- obsqura_assignments/src/main/java/interfaceassignment/RBIInterface.java
+++ obsqura_assignments/src/main/java/interfaceassignment/RBIInterface.java
@@ -2,7 +2,7 @@ package interfaceassignment;
 
 public interface RBIInterface {
 
-public abstract double recurringDeposit(double Principle, double n);//n-period
+public double recurringDeposit(double Principle, double n);//n-period
 double r = 11.0; // rate of interest
 
 }
